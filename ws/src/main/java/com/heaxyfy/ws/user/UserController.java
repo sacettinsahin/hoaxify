a5926d288e2f55ab9c7e815accd99a0c8eeb74d7
Sacettin Şahin <93089940+sacettinsahin@users.noreply.github.com>
2024-02-11 16:07:40 +0300

    Validations added for ws
    Username and email validations were added to user controller.

--- ws/src/main/java/com/heaxyfy/ws/user/UserController.java
+++ ws/src/main/java/com/heaxyfy/ws/user/UserController.java
@@ -1,11 +1,15 @@
 package com.heaxyfy.ws.user;
 
+import com.heaxyfy.ws.error.ApiError;
 import com.heaxyfy.ws.shared.GenericMessage;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @AllArgsConstructor
@@ -15,8 +19,30 @@ public class UserController {
 
 
     @PostMapping("/api/v1/users")
-    GenericMessage createUser(@RequestBody User user){
+    ResponseEntity<?> createUser(@RequestBody User user){
+
+        ApiError apiError = new ApiError();
+        apiError.setPath("/api/v1/users");
+        apiError.setMessage("Validation error");
+        apiError.setStatus(400);
+        Map<String,String> validationErrors = new HashMap<>();
+
+        if(user.getUsername() == null || user.getUsername().isEmpty()){
+            validationErrors.put("username","Username cannot be null");
+        }
+
+        if(user.getEmail() == null || user.getEmail().isEmpty()){
+            validationErrors.put("email", "Email cannot be null");
+        }
+
+        if(validationErrors.size() > 0){
+            apiError.setValidationErrors(validationErrors);
+            return ResponseEntity.badRequest().body(apiError);
+        }
+
+
+
         userService.saveUser(user);
-        return new GenericMessage("User is created.");
+        return ResponseEntity.ok(new GenericMessage("User is created"));
     }
 }
