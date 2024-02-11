97b75085f647b58757bee30eef9f09b8bf2f338a
Sacettin Şahin <93089940+sacettinsahin@users.noreply.github.com>
2024-02-11 16:41:33 +0300

    Exception Handler added to controller.

--- ws/src/main/java/com/heaxyfy/ws/user/UserController.java
+++ ws/src/main/java/com/heaxyfy/ws/user/UserController.java
@@ -2,11 +2,12 @@ package com.heaxyfy.ws.user;
 
 import com.heaxyfy.ws.error.ApiError;
 import com.heaxyfy.ws.shared.GenericMessage;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -19,30 +20,27 @@ public class UserController {
 
 
     @PostMapping("/api/v1/users")
-    ResponseEntity<?> createUser(@RequestBody User user){
+    ResponseEntity<?> createUser(@Valid @RequestBody User user){
+        userService.saveUser(user);
+        return ResponseEntity.ok(new GenericMessage("User is created"));
+    }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    ResponseEntity<ApiError> handleMethodNotValidEx(MethodArgumentNotValidException exception){
         ApiError apiError = new ApiError();
         apiError.setPath("/api/v1/users");
         apiError.setMessage("Validation error");
         apiError.setStatus(400);
         Map<String,String> validationErrors = new HashMap<>();
 
-        if(user.getUsername() == null || user.getUsername().isEmpty()){
-            validationErrors.put("username","Username cannot be null");
-        }
+        for (var fieldError: exception.getBindingResult().getFieldErrors()){
+         validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage() );
+       }
 
-        if(user.getEmail() == null || user.getEmail().isEmpty()){
-            validationErrors.put("email", "Email cannot be null");
-        }
-
-        if(validationErrors.size() > 0){
-            apiError.setValidationErrors(validationErrors);
-            return ResponseEntity.badRequest().body(apiError);
-        }
-
-
-
-        userService.saveUser(user);
-        return ResponseEntity.ok(new GenericMessage("User is created"));
+        apiError.setValidationErrors(validationErrors);
+        return ResponseEntity.badRequest().body(apiError);
+        
     }
+
+
 }
