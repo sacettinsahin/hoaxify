0729c8d4985c37bab530f2665beeb932f115e6fe
Sacettin Şahin <93089940+sacettinsahin@users.noreply.github.com>
2024-02-11 16:41:18 +0300

    Entity validations added

--- ws/src/main/java/com/heaxyfy/ws/user/User.java
+++ ws/src/main/java/com/heaxyfy/ws/user/User.java
@@ -5,6 +5,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,7 +20,16 @@ public class User {
     @Id
     @GeneratedValue
     long id;
+
+    @NotBlank
+    @Size(min = 4, max = 255)
     String username;
+
+    @NotBlank
+    @Email
     String email;
+
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*//d).*$")
+    @Size(min = 8, max = 255)
     String password;
 }
